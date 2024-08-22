28be6538779906870d114e89954ab13733d0ff1f
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2024-08-22 23:24:46 +0530

    create another table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,15 @@ package org.example;
 import org.example.config.FactoryConfiguration;
 import org.example.embed.FullName;
 import org.example.entity.Student;
+import org.example.entity.Teacher;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
         Session session = FactoryConfiguration.getInstance().getSession();
-
-        FullName fullName = new FullName("Harsha","Nimeda");
+        //----------Student Table------------------------//
+      /*  FullName fullName = new FullName("Harsha","Nimeda");
         Student student = new Student();
         student.setId(6);
         student.setName(fullName);
@@ -29,6 +30,26 @@ public class Main {
         System.out.println(s1.getName().getFirstName());
         System.out.println(s1.getName().getLastName());
         System.out.println(s1.getAddress());
+        transaction.commit();
+        session.close();*/
+
+        //---------------Teacher Table----------------------//
+        FullName fullname = new FullName("Udara","San");
+        Teacher teacher = new Teacher();
+        teacher.setId(1);
+        teacher.setName(fullname);
+        teacher.setAddress("Colombo");
+        Transaction transaction = session.beginTransaction();
+        //session.save(teacher);
+       //session.update(teacher);
+        //session.delete("1",teacher);
+        //-----------Get Data---------------
+        Teacher t1 = session.get(teacher.getClass(),1);
+        System.out.println(t1.getId());
+        System.out.println(t1.getName().getFirstName());
+        System.out.println(t1.getName().getLastName());
+        System.out.println(t1.getAddress());
+
         transaction.commit();
         session.close();
     }
--- src/main/java/org/example/config/FactoryConfiguration.java
+++ src/main/java/org/example/config/FactoryConfiguration.java
@@ -2,6 +2,7 @@ package org.example.config;
 
 
 import org.example.entity.Student;
+import org.example.entity.Teacher;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,7 +10,9 @@ import org.hibernate.cfg.Configuration;
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private FactoryConfiguration() {
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+        Configuration configuration = new Configuration();
+        configuration.configure().addAnnotatedClass(Student.class);
+        configuration.configure().addAnnotatedClass(Teacher.class);
         sessionFactory= (SessionFactory) configuration.buildSessionFactory();
 
     }
